adb4eead35c6d4376b64eec8c67b9b0804eca766
rakvat <rakvat@users.noreply.github.com>
2017-01-01 12:25:07 -0800

    improved enter activity

--- app/src/main/java/com/example/rakvat/iyana/EnterDataActivity.java
+++ app/src/main/java/com/example/rakvat/iyana/EnterDataActivity.java
@@ -1,44 +1,58 @@
 package com.example.rakvat.iyana;
 
 import android.content.ContentValues;
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
-
-import static android.R.attr.value;
-import static android.R.id.message;
+import java.util.Map;
 
 public class EnterDataActivity extends AppCompatActivity {
+
+    private static final int MOOD_ID = 100;
+
+    private ArrayList<Integer> mColumns = new ArrayList<Integer>();
+
+    private static final Map<Integer, Integer> viewId2ValueMap;
+    static {
+        Map<Integer, Integer> aMap = new HashMap<Integer, Integer>();
+        aMap.put(R.id.plusplus, 5);
+        aMap.put(R.id.plus, 4);
+        aMap.put(R.id.plusminus, 3);
+        aMap.put(R.id.minus, 2);
+        aMap.put(R.id.minusminus, 1);
+        viewId2ValueMap = Collections.unmodifiableMap(aMap);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_data);
 
-        setFactorTitles();
-    }
-
-    public void setFactorTitles() {
-        int view_ids[] = { R.id.data_label0, R.id.data_label1,
-                           R.id.data_label2, R.id.data_label3,
-                           R.id.data_label4, R.id.data_label5,
-                           R.id.data_label6, R.id.data_label7,
-                           R.id.data_label8, R.id.data_label9 };
         List<String> titles = FactorTitleHelper.getFactorTitles(this);
-        for (int i = 0; i < FactorTitleHelper.MAX_FACTORS; i++) {
-           if (titles.get(i).length() > 0) {
-                TextView textView = (TextView) findViewById(view_ids[i]);
-                textView.setText(titles.get(i) + ":");
+        ViewGroup parent = (ViewGroup) findViewById(R.id.enter_data_list);
+        LayoutInflater inflater = getLayoutInflater();
+
+        inflateEnterValue(inflater, parent, getString(R.string.enter_data_mood_label), MOOD_ID);
+
+        for (int i = 0; i < titles.size(); i++) {
+            if (titles.get(i) == null || titles.get(i) == "") {
+                continue;
             }
+            mColumns.add(i);
+            inflateEnterValue(inflater, parent, Util.capitalize(titles.get(i)), i);
         }
     }
 
@@ -51,20 +65,13 @@ public class EnterDataActivity extends AppCompatActivity {
         values.put(DatabaseContract.MoodEntry.COLUMN_NAME_DATE, new Date().getTime());
 
         storeTextFrom(R.id.data_note, DatabaseContract.MoodEntry.COLUMN_NAME_NOTE, values);
-        storeValueFrom(R.id.data_mood, DatabaseContract.MoodEntry.COLUMN_NAME_MOOD, values);
-
-        int view_ids[] = { R.id.data0, R.id.data1, R.id.data2, R.id.data3, R.id.data4,
-                           R.id.data5, R.id.data6, R.id.data7, R.id.data8, R.id.data9};
-        for(int i = 0; i < view_ids.length; i++) {
-            storeValueFrom(view_ids[i], DatabaseContract.MoodEntry.FACTOR_COLUMNS[i], values);
+        storeValueFrom(MOOD_ID, DatabaseContract.MoodEntry.COLUMN_NAME_MOOD, values);
+        for(int i = 0; i < DatabaseContract.MoodEntry.FACTOR_COLUMNS.length; i++) {
+            if (mColumns.contains(i)) {
+                storeValueFrom(i, DatabaseContract.MoodEntry.FACTOR_COLUMNS[i], values);
+            }
         }
-
-        // Insert the new row, returning the primary key value of the new row
-        long newRowId;
-        newRowId = db.insert(
-                DatabaseContract.MoodEntry.TABLE_NAME,
-                null,
-                values);
+        db.insert(DatabaseContract.MoodEntry.TABLE_NAME, null, values);
 
         Toast toast = Toast.makeText(this, R.string.data_saved, Toast.LENGTH_SHORT);
         toast.show();
@@ -72,10 +79,10 @@ public class EnterDataActivity extends AppCompatActivity {
     }
 
     private void storeValueFrom(int view_id, String dbColumn, ContentValues values) {
-        EditText editText = (EditText) findViewById(view_id);
-        String s = editText.getText().toString();
-        if (!s.equals("")) {
-            int value = Integer.parseInt(s);
+        RadioGroup radioButtonGroup = (RadioGroup) findViewById(view_id).findViewById(R.id.radio_group);
+        int radioButtonID = radioButtonGroup.getCheckedRadioButtonId();
+        if (radioButtonID != -1) {
+            int value = viewId2ValueMap.get(radioButtonID);
             values.put(dbColumn, value);
         }
     }
@@ -86,5 +93,13 @@ public class EnterDataActivity extends AppCompatActivity {
         if (!s.equals("")) {
             values.put(dbColumn, s);
         }
+    }
+
+    private void inflateEnterValue(LayoutInflater inflater, ViewGroup parent, String title, int id) {
+        View rowView = inflater.inflate(R.layout.enter_data_row, null);
+        TextView label = (TextView) rowView.findViewById(R.id.label);
+        label.setText(title);
+        rowView.setId(id);
+        parent.addView(rowView);
     }
 }
---
+++ app/src/main/java/com/example/rakvat/iyana/Util.java
@@ -0,0 +1,11 @@
+package com.example.rakvat.iyana;
+
+/**
+ * Created by rakvat on 01/01/17.
+ */
+
+public class Util {
+    public static String capitalize(String str) {
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
+}
