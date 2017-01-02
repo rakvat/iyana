2662571de80fa8068bda79f4ae5391d014eb67be
rakvat <rakvat@users.noreply.github.com>
2017-01-02 05:07:52 -0800

    some stylings

--- app/src/main/java/com/example/rakvat/iyana/TimeChartActivity.java
+++ app/src/main/java/com/example/rakvat/iyana/TimeChartActivity.java
@@ -9,20 +9,18 @@ import android.os.Bundle;
 import com.github.mikephil.charting.charts.Chart;
 import com.github.mikephil.charting.charts.ScatterChart;
 import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.ScatterData;
 import com.github.mikephil.charting.data.ScatterDataSet;
-import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.IScatterDataSet;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-
-import static android.R.attr.left;
-import static android.R.attr.shape;
 
 
 public class TimeChartActivity extends AppCompatActivity {
@@ -39,7 +37,11 @@ public class TimeChartActivity extends AppCompatActivity {
             Color.rgb(118, 118, 118),
             Color.rgb(200, 200, 200),
     };
-    public static final int MAX_X_RANGE = 1000 * 60 * 60 * 24 * 7; // 7 days
+    private static final int MAX_X_RANGE = 1000 * 60 * 60 * 24 * 7; // 7 days
+    private static final int X_OFFSET = 1000 * 60 * 60 * 24 * 2;
+    private static final float MARKER_OFFSET = 0.1f;
+
+    private List<Integer> mValueCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -113,14 +115,18 @@ public class TimeChartActivity extends AppCompatActivity {
                 timeReference = timestamp;
             }
             long t = timestamp - timeReference;
-            moodEntries.add(new Entry(t, cursor.getInt(
-                    cursor.getColumnIndexOrThrow(DatabaseContract.MoodEntry.COLUMN_NAME_MOOD))));
+            int value = cursor.getInt(
+                    cursor.getColumnIndexOrThrow(DatabaseContract.MoodEntry.COLUMN_NAME_MOOD));
+            mValueCounter = new ArrayList<Integer>(Arrays.asList(0, 0, 0, 0, 0));
+            mValueCounter.set(value - 1, mValueCounter.get(value - 1) + 1);
+            moodEntries.add(new Entry(t, value + mValueCounter.get(value - 1) * MARKER_OFFSET));
             for (int i = 0; i < FactorTitleHelper.MAX_FACTORS; i++) {
                 if (titles.get(i) != null) {
-                    int value = cursor.getInt(
+                    value = cursor.getInt(
                             cursor.getColumnIndexOrThrow(DatabaseContract.MoodEntry.FACTOR_COLUMNS[i]));
                     if (value != 0) {
-                        entries.get(i).add(new Entry(t, value));
+                        mValueCounter.set(value - 1, mValueCounter.get(value - 1) + 1);
+                        entries.get(i).add(new Entry(t, value + mValueCounter.get(value - 1) * MARKER_OFFSET));
                     }
                 }
             }
@@ -132,12 +138,13 @@ public class TimeChartActivity extends AppCompatActivity {
     private void setScatterData(Chart chart, List<String> titles, List<Entry> moodEntries, List<List<Entry>> entries) {
         List<IScatterDataSet> dataSets = new ArrayList<IScatterDataSet>();
         ScatterDataSet dataSet = new ScatterDataSet(moodEntries, "Mood");
-        styleDataSet(dataSet, Color.rgb(0, 0, 0));
+        styleDataSet(dataSet, Color.rgb(0, 0, 0), ScatterChart.ScatterShape.SQUARE);
         dataSets.add(dataSet);
 
         for (int i = 0; i < FactorTitleHelper.MAX_FACTORS; i++) {
             if (titles.get(i) != null && entries.get(i).size() > 0) {
-                ScatterDataSet factorDataSet = new ScatterDataSet(entries.get(i), titles.get(i));
+                ScatterDataSet factorDataSet =
+                        new ScatterDataSet(entries.get(i), Util.capitalize(titles.get(i)));
                 styleDataSet(factorDataSet, COLORS[i]);
                 dataSets.add(factorDataSet);
             }
@@ -147,35 +154,45 @@ public class TimeChartActivity extends AppCompatActivity {
     }
 
     private void styleDataSet(ScatterDataSet dataSet, int color) {
+        styleDataSet(dataSet, color, ScatterChart.ScatterShape.CIRCLE);
+    }
+
+    private void styleDataSet(ScatterDataSet dataSet, int color, ScatterChart.ScatterShape shape) {
         dataSet.setColor(color);
         dataSet.setDrawValues(false);
         dataSet.setScatterShapeSize(20);
-        dataSet.setScatterShape(ScatterChart.ScatterShape.CIRCLE);
+        dataSet.setScatterShape(shape);
     }
 
     private void styleChart(ScatterChart chart, long timeReference) {
-        IAxisValueFormatter xAxisFormatter = new DateAxisValueFormatter(timeReference);
         XAxis xAxis = chart.getXAxis();
-        xAxis.setValueFormatter(xAxisFormatter);
+        xAxis.setValueFormatter(new DateAxisValueFormatter(timeReference));
+        xAxis.setDrawGridLines(false);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
 
         YAxis right = chart.getAxisRight();
         right.setDrawLabels(false);
         right.setDrawAxisLine(true);
         right.setDrawGridLines(false);
         YAxis yAxis = chart.getAxisLeft();
-        yAxis.setAxisMinimum(1);
-        yAxis.setAxisMaximum(5);
+        yAxis.setAxisMinimum(0.0f);
+        yAxis.setAxisMaximum(6.0f);
         yAxis.setTextColor(Color.BLACK);
         yAxis.setGranularity(1);
-        yAxis.setLabelCount(5, true);
+        yAxis.setLabelCount(7, true);
+        yAxis.setDrawGridLines(false);
+        yAxis.setValueFormatter(new YAxisValueFormatter());
 
         // Hide the description
         Description d = new Description();
         d.setText("");
         chart.setDescription(d);
 
+        Legend legend = chart.getLegend();
+        legend.setWordWrapEnabled(true);
+
         chart.setVisibleXRangeMaximum(MAX_X_RANGE);
-        chart.moveViewToX(new Date().getTime() - timeReference - MAX_X_RANGE);
+        chart.moveViewToX(new Date().getTime() - timeReference - MAX_X_RANGE + X_OFFSET);
 
         //MyMarkerView myMarkerView= new MyMarkerView(getApplicationContext(), R.layout.my_marker_view_layout, timeReference);
         //chart.setMarkerView(myMarkerView);
---
+++ app/src/main/java/com/example/rakvat/iyana/YAxisValueFormatter.java
@@ -0,0 +1,33 @@
+package com.example.rakvat.iyana;
+
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class YAxisValueFormatter implements IAxisValueFormatter {
+
+    private static final Map<Integer, String> value2SignMap;
+    static {
+        Map<Integer, String> aMap = new HashMap<Integer, String>();
+        aMap.put(1, "--");
+        aMap.put(2, "-");
+        aMap.put(3, "+-");
+        aMap.put(4, "+");
+        aMap.put(5, "++");
+        value2SignMap = Collections.unmodifiableMap(aMap);
+    }
+
+    @Override
+    public String getFormattedValue(float value, AxisBase axis)
+    {
+        if (!value2SignMap.containsKey((int)value)) {
+            return "";
+        }
+        return value2SignMap.get((int)value);
+    }
+}
