2d1f0c5337f2e0bef06af977ac1e6088c130b424
rakvat <rakvat@users.noreply.github.com>
2017-01-13 03:46:32 -0800

    cleanup and 1st verion of help text

--- app/src/main/java/com/example/rakvat/iyana/AboutActivity.java
+++ app/src/main/java/com/example/rakvat/iyana/AboutActivity.java
@@ -1,10 +1,13 @@
 package com.example.rakvat.iyana;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -13,6 +16,14 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         Util.setTitleBar(this, R.string.nav_about);
+        try {
+            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            String version = pInfo.versionName;
+            TextView versionTextView = (TextView)findViewById(R.id.versionTextView);
+            versionTextView.setText("This is version " + version + " of IYANA.");
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
