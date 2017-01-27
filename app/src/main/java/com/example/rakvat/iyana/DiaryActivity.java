a5fded6ed590712fe339f876488ed80d595ac649
rakvat <rakvat@users.noreply.github.com>
2017-01-27 03:44:42 -0800

    started with diary view

--- app/src/main/java/com/example/rakvat/iyana/DiaryActivity.java
+++ app/src/main/java/com/example/rakvat/iyana/DiaryActivity.java
@@ -11,16 +11,34 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
-import static android.R.attr.id;
 
 public class DiaryActivity extends AppCompatActivity {
+
+    private static final int MAX_DAYS = 7;
+
+
+    private static final Map<Integer, Integer> value2ViewIdMap;
+    static {
+        Map<Integer, Integer> aMap = new HashMap<Integer, Integer>();
+        aMap.put(5, R.id.plusplus);
+        aMap.put(4, R.id.plus);
+        aMap.put(3, R.id.plusminus);
+        aMap.put(2, R.id.minus);
+        aMap.put(1, R.id.minusminus);
+        value2ViewIdMap = Collections.unmodifiableMap(aMap);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +47,9 @@ public class DiaryActivity extends AppCompatActivity {
         Util.setTitleBar(this, R.string.nav_diary);
 
 
-        ViewGroup parent = (ViewGroup) findViewById(R.id.diary_entry_list);
+        ViewGroup parent = (ViewGroup) findViewById(R.id.diary_rows);
         LayoutInflater inflater = getLayoutInflater();
-        Cursor cursor = Util.getDBData(this);
+        Cursor cursor = Util.getDBData(this, MAX_DAYS);
 
         while(cursor.moveToNext()) {
             long timestamp = cursor.getLong(
@@ -43,6 +61,33 @@ public class DiaryActivity extends AppCompatActivity {
             Date date = new Date();
             date.setTime(timestamp);
             dateView.setText(format.format(date));
+
+            String note = cursor.getString(
+                    cursor.getColumnIndexOrThrow(DatabaseContract.MoodEntry.COLUMN_NAME_NOTE));
+            TextView noteView = (TextView) rowView.findViewById(R.id.note);
+            if (note != null && note != "") {
+                noteView.setText(note);
+                noteView.setVisibility(View.VISIBLE);
+            }
+
+            List<String> titles = FactorTitleHelper.getFactorTitles(this);
+            for (int i = 0; i < FactorTitleHelper.MAX_FACTORS; i++) {
+                if (titles.get(i) != null) {
+                    int value = cursor.getInt(
+                            cursor.getColumnIndexOrThrow(DatabaseContract.MoodEntry.FACTOR_COLUMNS[i]));
+                    if (value != 0) {
+                        ViewGroup rowParent = (ViewGroup) rowView.findViewById(R.id.diary_row_entries);
+                        View rowEntryView = inflater.inflate(R.layout.diary_row_entry, null);
+                        TextView labelView = (TextView) rowEntryView.findViewById(R.id.label);
+                        labelView.setText(Util.capitalize(titles.get(i)));
+
+                        TextView valueView = (TextView) rowEntryView.findViewById(value2ViewIdMap.get(value));
+                        valueView.setVisibility(View.VISIBLE);
+
+                        rowParent.addView(rowEntryView);
+                    }
+                }
+            }
             rowView.setId((int) (long)timestamp);
             parent.addView(rowView);
         }
@@ -75,6 +120,13 @@ public class DiaryActivity extends AppCompatActivity {
 
     /** Called when the user clicks an edit button */
     public void edit(View view) {
+        Toast toast = Toast.makeText(this, R.string.todo, Toast.LENGTH_SHORT);
+        toast.show();
+    }
 
+    /** Called when the user clicks a delete button */
+    public void delete(View view) {
+        Toast toast = Toast.makeText(this, R.string.todo, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
